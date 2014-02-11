3ff9d33b98fc888e15508095329f6869b8e81b2c
AntiCod <xpund@yandex.ru>
2014-02-11 23:44:03 +0700

    DB/Spell: Fix: "Raise Death" of the Death knight Class (Thx Torrad).

---
+++ sql/updates/xpund_update/2014_02_11_00_world_spell_raise_death.sql
@@ -0,0 +1,4 @@
+-- DB/Spell: Fix: "Raise Death" of the Death knight Class | by Torrad
+DELETE FROM `spell_script_names` WHERE `spell_id` = -46584;
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
+(-46584, "spell_dk_raise_dead");
